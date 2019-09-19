b0fbec23f4c707b3d057afb068648bf983b77546
Lai Peng Hung <101208371@students.swinburne.edu.my>
2019-09-19 14:45:12 +0800

    add game timer logic to Snap and have the game started when the spacebar is pressed

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -24,7 +24,7 @@ namespace CardGames
 
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
-				myGame.FlipNextCard ();
+				myGame.Start ();
 			}
 		}
 
@@ -34,7 +34,7 @@ namespace CardGames
 		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
 		private static void DrawGame(Snap myGame)
 		{
-			SwinGame.ClearScreen(Color.White);
+			SwinGame.DrawBitmap("cardsBoard.png",0,0);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
